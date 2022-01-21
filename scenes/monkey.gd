aa430ae0336d90e15c5e0a0e4f88ffed0891f81e
Toby Stoodley <tobiahstoodley@gmail.com>
2022-01-21 12:20:11 +0000

    spikes added, todo fix collision

--- scenes/monkey.gd
+++ scenes/monkey.gd
@@ -9,9 +9,15 @@ func _process(delta):
 	if Input.is_key_pressed(KEY_LEFT):
 		apply_central_impulse(Vector2(-3,0))
 		if $Feet.get_overlapping_bodies().size()>0:
-			apply_central_impulse(Vector2(-7,0))
+			apply_central_impulse(Vector2(-7,3))
 			
 	if Input.is_key_pressed(KEY_RIGHT):
 		apply_central_impulse(Vector2(3,0))
 		if $Feet.get_overlapping_bodies().size()>0:
-			apply_central_impulse(Vector2(7,0))
+			apply_central_impulse(Vector2(7,3))
+	var areas = get_overlapping_bodies()
+	if areas.size()>0:
+		for x in areas:
+			if x.collision_layer & 1 << 5:
+				print("Hit")
+			
