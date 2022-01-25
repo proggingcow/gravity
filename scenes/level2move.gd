f1fde2d786e5367ebf31280890d8a9165c695a8d
Toby Stoodley <tobiahstoodley@gmail.com>
2022-01-25 10:44:47 +0000

    levels now work with exit

---
+++ scenes/level2move.gd
@@ -0,0 +1,21 @@
+extends Area2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Area2D5_area_shape_entered(area_rid, area, area_shape_index, local_shape_index):
+	var game = preload ("level2.tscn"). instance ()
+	hide()
--- scenes/monkey.gd
+++ scenes/monkey.gd
@@ -5,7 +5,7 @@ func _input(event):
 		if event.pressed and event.scancode == KEY_UP and ($Feet.get_overlapping_bodies().size() > 0) :
 				apply_central_impulse(Vector2(0,-200))
 
-func _process(delta):
+func _process(_delta):
 	if Input.is_key_pressed(KEY_LEFT):
 		apply_central_impulse(Vector2(-3,0))
 		if $Feet.get_overlapping_bodies().size()>0:
@@ -15,9 +15,14 @@ func _process(delta):
 		apply_central_impulse(Vector2(3,0))
 		if $Feet.get_overlapping_bodies().size()>0:
 			apply_central_impulse(Vector2(7,3))
-	var areas = get_overlapping_bodies()
-	if areas.size()>0:
-		for x in areas:
-			if x.collision_layer & 1 << 5:
-				print("Hit")
-			
+
+func _on_Suround_area_entered(_b):
+	get_tree().reload_current_scene()
+	
+#var game = preload ("the path to your scene"). instance ()
+#gettree (). getroot (). add_child (game)
+#hide ()
+
+
+func _on_Area2D5_area_entered(area):
+	pass # Replace with function body.
