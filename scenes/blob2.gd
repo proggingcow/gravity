60aec014f29ffcb0d3c45af54d5277c88eecf2e8
proggingcow <tobiahstoodley@gmail.com>
2022-06-28 11:42:25 +0100

    level 20 complete

--- scenes/blob2.gd
+++ scenes/blob2.gd
@@ -1,12 +1,5 @@
 extends RigidBody2D
 
-func _input(event):
-	if event is InputEventKey :
-		if event.pressed and event.scancode == KEY_W and ($Feet.get_overlapping_bodies().size() > 0) :
-			apply_central_impulse(Vector2(0,-200))
-			$AudioStreamPlayer2D.play()
-		if event.pressed and event.scancode == KEY_Q:
-			get_tree().quit()
 
 func _physics_process(delta):
 	if linear_velocity.y < - 210:
@@ -26,8 +19,12 @@ func _process(_delta):
 		apply_central_impulse(Vector2(0,1))
 		if $water_zone.get_overlapping_areas().size() != 0:
 			apply_central_impulse(Vector2(0,3))
-	if Input.is_key_pressed(KEY_W) and $water_zone.get_overlapping_areas().size() != 0:
-		apply_central_impulse(Vector2(0,-4))
+	if Input.is_key_pressed(KEY_W): 
+		if $water_zone.get_overlapping_areas().size() != 0:
+			apply_central_impulse(Vector2(0,-4))
+		elif ($Feet.get_overlapping_bodies().size() > 0):
+			apply_central_impulse(Vector2(0,-180))
+			$AudioStreamPlayer2D.play()
 	if Input.is_key_pressed(KEY_D):
 		apply_central_impulse(Vector2(3,0))
 		if $Sprite3.scale.x == 1:
--- scenes/monkey.gd
+++ scenes/monkey.gd
@@ -1,24 +1,5 @@
 extends RigidBody2D
 
-func _ready():
-	var date = OS.get_date()
-	if date.month == 5:
-		$Sprite3.texture = load("res://scenes/assets/blobcat.png")
-	if date.month == 2:
-		$Sprite3.texture = load("res://scenes/assets/blobred.png")
-	if date.month == 8:
-		$Sprite3.texture = load("res://scenes/assets/blobthebonkers.png")
-	if date.month == 10:
-		$Sprite3.texture = load("res://scenes/assets/pandablob.png")
-
-func _input(event):
-	if event is InputEventKey :
-		if event.pressed and event.scancode == KEY_UP and ($Feet.get_overlapping_bodies().size() > 0) :
-			apply_central_impulse(Vector2(0,-200))
-			$AudioStreamPlayer2D.play()
-		if event.pressed and event.scancode == KEY_Q:
-			get_tree().quit()
-
 func _physics_process(delta):
 	if linear_velocity.y < - 210:
 		linear_velocity.y = -210
@@ -37,18 +18,20 @@ func _process(_delta):
 		apply_central_impulse(Vector2(0,1))
 		if $water_zone.get_overlapping_areas().size() != 0:
 			apply_central_impulse(Vector2(0,3))
-	if Input.is_key_pressed(KEY_UP) and $water_zone.get_overlapping_areas().size() != 0:
-		apply_central_impulse(Vector2(0,-4))
+	if Input.is_key_pressed(KEY_UP):
+		if $water_zone.get_overlapping_areas().size() != 0:
+			apply_central_impulse(Vector2(0,-4))
+		elif ($Feet.get_overlapping_bodies().size() > 0):
+			apply_central_impulse(Vector2(0,-180))
+			$AudioStreamPlayer2D.play()
 	if Input.is_key_pressed(KEY_RIGHT):
 		apply_central_impulse(Vector2(3,0))
 		if $Sprite3.scale.x == 1:
 			$Sprite3.scale.x = -1
 		if $Feet.get_overlapping_bodies().size()>0:
 			apply_central_impulse(Vector2(7,3))
+	if Input.is_key_pressed(KEY_Q):
+		get_tree().quit()
 
 func _on_Suround_area_entered(_b):
 	get_tree().reload_current_scene()
-
-
-func _on_Area2D5_area_entered(area):
-	pass # Replace with function body.
