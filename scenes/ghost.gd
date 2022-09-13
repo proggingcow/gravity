a75ea66781bdffaaa4f4b815373fa1ea52e5151b
proggingcow <tobiahstoodley@gmail.com>
2022-09-13 12:09:50 +0100

    finale progress

--- scenes/ghost.gd
+++ scenes/ghost.gd
@@ -6,7 +6,7 @@ func _process(_delta):
 	position.x += move
 	if $left.get_overlapping_bodies():
 		move = abs(move)
-		$Sprite.scale.x = -1
+		$Sprite.scale.x = -0.5
 	if $right.get_overlapping_bodies():
 		move = -abs(move)
-		$Sprite.scale.x = 1
+		$Sprite.scale.x = 0.5
