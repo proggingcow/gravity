233ac0250b7c4f08174caac6c46235328280924d
proggingcow <tobiahstoodley@gmail.com>
2022-09-20 11:41:36 +0100

    finale progress

--- scenes/info.gd
+++ scenes/info.gd
@@ -1,21 +1,17 @@
-extends Node2D
+extends RichTextLabel
 
 
 # Declare member variables here. Examples:
 # var a = 2
-var p = 0
+# var b = "text"
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	p = $blob.position.y
-	if p >0:
-		$blob/c1.current = true
-	elif p >-600:
-		$blob/c2.current = true
-
+	if !get_tree().paused:
+		visible = false
